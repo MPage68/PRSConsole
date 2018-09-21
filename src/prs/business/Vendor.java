963f7e4edc30132eb25d7aa2b219a1d957a2b6fa
mpage68 <mikepage68@live.com>
2018-09-20 21:03:26 -0400

    finished vendor class

--- src/prs/business/Vendor.java
+++ src/prs/business/Vendor.java
@@ -1,16 +1,127 @@
 package prs.business;
 
 public class Vendor {
-	 ID INT PRIMARY KEY AUTO_INCREMENT,
-	    Code VARCHAR(10) NOT NULL UNIQUE,
-	    Name VARCHAR(255) NOT NULL,
-	    Address VARCHAR(255) NOT NULL,
-	    City VARCHAR(255) NOT NULL,
-	    State VARCHAR(2) NOT NULL,
-	    Zip VARCHAR(5) NOT NULL,
-	    PhoneNumber VARCHAR(12) NOT NULL,
-	    Email VARCHAR(100) NOT NULL,
-	    IsPreApproved TINYINT(1) NOT NULL,
-	    IsActive TINYINT(1),
+	private int ID;
+	private String code;
+	private String name;
+	private String address;
+	private String city;
+	private String state;
+	private String zip;
+	private String phoneNumber;
+	private String email;
+	private boolean isPreApproved;
+	private boolean isActive;
+
+	public Vendor(int iD, String code, String name, String address, String city, String state, String zip,
+			String phoneNumber, String email, boolean isPreApproved, boolean isActive) {
+		super();
+		ID = iD;
+		this.code = code;
+		this.name = name;
+		this.address = address;
+		this.city = city;
+		this.state = state;
+		this.zip = zip;
+		this.phoneNumber = phoneNumber;
+		this.email = email;
+		this.isPreApproved = isPreApproved;
+		this.isActive = isActive;
+	}
+
+	public int getID() {
+		return ID;
+	}
+
+	public void setID(int iD) {
+		ID = iD;
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getZip() {
+		return zip;
+	}
+
+	public void setZip(String zip) {
+		this.zip = zip;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public boolean isPreApproved() {
+		return isPreApproved;
+	}
+
+	public void setPreApproved(boolean isPreApproved) {
+		this.isPreApproved = isPreApproved;
+	}
+
+	public boolean isActive() {
+		return isActive;
+	}
+
+	public void setActive(boolean isActive) {
+		this.isActive = isActive;
+	}
+
+	@Override
+	public String toString() {
+		return "Vendor [ID=" + ID + ", code=" + code + ", name=" + name + ", address=" + address + ", city=" + city
+				+ ", state=" + state + ", zip=" + zip + ", phoneNumber=" + phoneNumber + ", email=" + email
+				+ ", isPreApproved=" + isPreApproved + ", isActive=" + isActive + "]";
+	}
 
 }
